fe226ba13b37271aacf29909f4ede2990db9d28e
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-11-11 11:17:37 -0500

    hompage to dashboard redirected to login page unless logged in

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import { useEffect } from 'react'                       
+import { useNavigate } from 'react-router-dom'        // to redirect user
+import { useSelector, useDispatch } from 'react-redux' // to grab user from the state to see if we are logged in or not
+// import GoalForm from '../components/GoalForm'
+// import GoalItem from '../components/GoalItem'
+// import Spinner from '../components/Spinner'
+// import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
+  const navigate = useNavigate()
+
+  const {user} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+  }, [user,navigate] )
+ 
   return (
     <div>Dashboard</div>
   )
